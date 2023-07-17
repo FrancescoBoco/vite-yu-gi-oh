79d2b1260633236132af2ddaacf1dd2dc594a4fe
FrancescoBoco <130904080+FrancescoBoco@users.noreply.github.com>
2023-07-17 15:28:33 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: "App",
+  components:{
+    Header,
+    Main,
+    Footer,
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <Header />
+
+  <Main/>
+   
+
+  <Footer/>
+</template>
+
+<style lang="scss" scoped>
+@use "./assets/scss/main.scss";
+</style>
